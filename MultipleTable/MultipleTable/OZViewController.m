315da1ecc913dfa4b240ebed6569bcb22668e70f
Chalermchon Samana <appcodev@gmail.com>
2013-09-20 09:32:50 +0700

    เพิ่ม offset เข้าไปด้วย ตารางสูตรคูณที่ไหนมี offset ด้วยหว่า ???

--- MultipleTable/MultipleTable/OZViewController.m
+++ MultipleTable/MultipleTable/OZViewController.m
@@ -11,9 +11,10 @@
 @interface OZViewController (){
     
     IBOutlet UITextField *numberText;
+    IBOutlet UITextField *offsetText;
     IBOutlet UITableView *tableResult;
     
-    NSNumber *number;
+    NSNumber *number,*offset;
 }
 
 @end
@@ -35,8 +36,9 @@
 
 - (IBAction)goMultiple:(UIButton *)sender {
     
-    if (![numberText.text isEqualToString:@""]) {
+    if (![numberText.text isEqualToString:@""] && ![offsetText.text isEqualToString:@""]) {
         number = @(numberText.text.intValue);
+        offset = @(offsetText.text.intValue);
         //NSLog(@"number %@",number);
         [tableResult reloadData];
     }
@@ -64,6 +66,10 @@
     UILabel *label3 = (UILabel*)[cell viewWithTag:3];
     int result = number.intValue*mul;
     label3.text = [NSString stringWithFormat:@"%d",result];
+    
+    UILabel *label4 = (UILabel*)[cell viewWithTag:4];
+    int resultOffset = result+offset.intValue;
+    label4.text = [NSString stringWithFormat:@"%d",resultOffset];
     
     return cell;
 }
